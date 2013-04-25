ba1e1a5ce29cc516ed58664ed6ba9ea71b838433
Charlie Austin <Charlie_Austin@gap.com>
2013-04-24 19:00:32 -0700

    Round 6

--- 2-Java/src/PacMan.java
+++ 2-Java/src/PacMan.java
@@ -1,5 +1,25 @@
 public class PacMan {
-   public PacMan(int x, int y) {
+   private int x;
+   private int y;
 
+   public PacMan(int x, int y) {
+      this.x = x;
+      this.y = y;
+   }
+
+   public int getX() {
+      return x;
+   }
+
+   public void move() {
+      x++;
+   }
+
+   public int getY() {
+      return y;
+   }
+
+   public Direction getDirection() {
+      return Direction.EAST;
    }
 }
--- 2-Java/test/PacManTest.java
+++ 2-Java/test/PacManTest.java
@@ -1,8 +1,27 @@
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class PacManTest {
+
+   @Test
+   public void shouldReturnCurrentPosition(){
+      PacMan p = new PacMan(5,6);
+      assertThat(p.getX(), is(5));
+      assertThat(p.getY(), is(6));
+   }
+
    @Test
    public void shouldBeAbleToMove(){
+      PacMan p = new PacMan(5,5);
+      p.move();
+      assertThat(p.getX(), is(6));
+   }
 
+   @Test
+   public void hasDirection() {
+      PacMan p = new PacMan(5,6);
+      assertThat(p.getDirection(), is(Direction.EAST));
    }
 }
